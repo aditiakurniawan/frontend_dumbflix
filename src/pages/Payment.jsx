2e028596970cc4d9b4450b2c8324fc74b71c55e8
aditiakurniawan <108774796+aditiakurniawan@users.noreply.github.com>
2022-09-25 15:59:14 +0700

    edit pakage

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -29,7 +29,8 @@ function Payment() {
     //change this to the script source you want to load, for example this is snap.js sandbox env
     const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
     //change this according to your client-key
-    const myMidtransClientKey = "SB-Mid-client-aAfwA9NGkcUy4r0-";
+    // const myMidtransClientKey = "SB-Mid-client-aAfwA9NGkcUy4r0-";
+    const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
 
     let scriptTag = document.createElement("script");
     scriptTag.src = midtransScriptUrl;
